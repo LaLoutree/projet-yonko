bc08541b0f4eb3e1a8eb830449f7c8d5840c2a81
LaLoutree <126497530+LaLoutree@users.noreply.github.com>
2023-03-03 12:08:08 +0100

    debut

--- Project Yonko/Assets/FirstPersonController.cs
+++ Project Yonko/Assets/FirstPersonController.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class FirstPersonController : MonoBehaviour
 {
     public bool canMove {get; private set;} = true;
+    private bool IsSprinting => canSprint && Input.GetKey(sprintKey);
+    private bool ShouldJump => Input.GetKeyDown(jumpKey) && characterController.isGrounded;
+
+    [Header("Functionnal Options")]
+    [SerializeField] private bool canSprint = true;
+    [SerializeField] private bool canJump = true;
+
+    [Header("Controls")]
+    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
 
     [Header("Movement parameters")]
     [SerializeField] private float walkSpeed = 3.0f;
-    [SerializeField] private float gravity = 30.0f;
+    [SerializeField] private float sprintSpeed = 6.0f;
 
     [Header("Look parameters")]
     [SerializeField, Range(1,10)] private float lookSpeedX = 2.0f;
     [SerializeField, Range(1,10)] private float lookSpeedY = 2.0f;
     [SerializeField, Range(1,180)] private float upperLookLimit = 80.0f;
     [SerializeField, Range(1,180)] private float lowerLookLimit = 80.0f;
+
+    [Header("Jump Parameters")]
+    [SerializeField] private float jumpForce = 8.0f;
+    [SerializeField] private float gravity = 30.0f;
 
     private Camera playerCamera;
     private CharacterController characterController;
@@ -40,23 +54,33 @@ public class FirstPersonController : MonoBehaviour
             HandleMovementInput();
             HandleMouseLook();
 
+            if (canJump)
+                HandleJump();
+            
             ApplyFinalMovement();
         }
     }
 
     private void HandleMovementInput()
     {
-        currentInput = new Vector2(walkSpeed * Input.GetAxis("Vertical"), walkSpeed * Input.GetAxis("Horizontal"));
-
+        currentInput = new Vector2((IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Vertical"), (IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Horizontal"));
         float moveDirectionY = moveDirection.y;
         moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) + (transform.TransformDirection(Vector3.right) * currentInput.y);
-
         moveDirection.y = moveDirectionY;
     }
 
     private void HandleMouseLook()
     {
+        rotationX  -=  Input.GetAxis("Mouse Y") * lookSpeedY;
+        rotationX = Mathf.Clamp(rotationX, -upperLookLimit, lowerLookLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeedX, 0);
+    }
 
+    private void HandleJump()
+    {
+        if (ShouldJump)
+            moveDirection.y = jumpForce;
     }
 
     private void ApplyFinalMovement()
